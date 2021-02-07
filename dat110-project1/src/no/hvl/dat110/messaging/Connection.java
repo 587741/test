9b0ca2bae7d3ff974d3724dea7d7e0e478ad9c1e
587741 <54102896+587741@users.noreply.github.com>
2021-02-07 19:30:28 +0100

    Update Connection.java

--- dat110-project1/src/no/hvl/dat110/messaging/Connection.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Connection.java
@@ -5,12 +5,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
+import no.hvl.dat110.TODO;
+
 public class Connection {
 
 	private DataOutputStream outStream; // for writing bytes to the underlying TCP connection
 	private DataInputStream inStream; // for reading bytes from the underlying TCP connection
 	private Socket socket; // socket for the underlying TCP connection
-	
+
 	public Connection(Socket socket) {
 
 		try {
@@ -33,43 +35,22 @@ public class Connection {
 		// TODO
 		// encapsulate the data contained in the message and write to the output stream
 		// Hint: use the encapsulate method on the message
-		
-		try {
-			
-			outStream = new DataOutputStream(socket.getOutputStream());
-			outStream.write(message.encapsulate());
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		
+		throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public Message receive() {
-		
-		Message message = null;
-		byte[] recvbuf = null ;
 
-		
-		try {
-			inStream = new DataInputStream(socket.getInputStream());
-			inStream.read(recvbuf);
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// TODO 
+		Message message;
+		byte[] recvbuf;
+
+		// TODO
 		// read a segment (128 bytes) from the input stream and decapsulate into message
 		// Hint: create a new Message object and use the decapsulate method
 		
-		message.decapsulate(recvbuf);
-		
+		if (true) {
+			throw new RuntimeException("not yet implemented");
+		}
 
 		return message;
 
